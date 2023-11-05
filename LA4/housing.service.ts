de4659a7b3c825cfae1b1ae3330978c1d1bb12b9
ahanachats <103726343+ahanachats@users.noreply.github.com>
2023-11-05 18:36:01 +0530

    Create housing.service.ts

---
+++ LA4/housing.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HousingService {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  url = 'http://localhost:3000/locations';
+
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+  console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+}
+}
