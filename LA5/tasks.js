553be2ec23cc8d50a08821614fe323efde7a08b0
ahanachats <103726343+ahanachats@users.noreply.github.com>
2023-11-05 18:39:27 +0530

    Create tasks.js

---
+++ LA5/tasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Task = ({ task, onDelete }) => { return (
+<div className="task">
+<span>{task}</span>
+<button onClick={() => onDelete(task)}>Delete</button>
+</div>
+);
+};
+
+export default Task;
